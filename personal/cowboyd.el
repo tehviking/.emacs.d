ec7a4260381750ac4e1517f8924c73c16cbba883
Charles Lowell <cowboyd@frontside.io>
2014-06-04 10:38:02 -0500

    use company for completion

--- personal/cowboyd.el
+++ personal/cowboyd.el
@@ -7,6 +7,18 @@
 ;; YAS snippets everywhere bro.
 (yas-global-mode)
 
+;; use the company-mode
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(push 'company-robe company-backends)
+(global-set-key (kbd "<C-s-268632064>") 'company-complete)
+
+;; Enable whitespace mode for code
+(add-hook 'prog-mode-hook 'prelude-enable-whitespace)
+(add-hook 'ruby-mode-hook 'prelude-enable-whitespace)
+(add-hook 'enh-ruby-mode-hook 'prelude-enable-whitespace)
+(add-hook 'coffee-mode-hook 'prelude-enable-whitespace)
+
 ;; enable colors in compilation buffer
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
@@ -20,14 +32,28 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 
+;; make the default search use Ag
+(define-key projectile-mode-map (kbd "s-g") 'projectile-ag)
 
 (add-hook 'prog-mode-hook
           (lambda () (local-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)))
 
 
 ;; make sure that handlebars templates load in web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;; Using web-mode for editing plain HTML files can be done this way
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("Gemfile" . enh-ruby-mode))
+
+
+;;(add-to-list 'auto-mode-alist '("Gemfile" . enh-ruby-mode))
 
 ;; smart parens configuration
 (require 'smartparens-config)
@@ -36,9 +62,14 @@
 ;; drag stuff rules!
 (drag-stuff-global-mode)
 
-;; I use enh-ruby-mode and not ruby mode, but I gotta have my snippets!
+;; I use enh-ruby-mode and not ruby mode, but I gotta have my snippets and my comment-or-uncomment!
 (add-hook 'enh-ruby-mode-hook
           (lambda() (yas-activate-extra-mode 'ruby-mode)))
+(add-hook 'enh-ruby-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)))
+
+
+
 
 ;; Disable smartparens-mode when using multiple cursors. they don't play well together
 (add-to-list 'mc/unsupported-minor-modes 'smartparens-mode)
@@ -46,3 +77,8 @@
 
 ;; Yay autocomplete
 (require 'auto-complete-config)
+(add-hook 'enh-ruby-mode-hook `auto-complete-mode)
+
+;; Twitter
+(setq twittering-icon-mode t)
+(setq twittering-use-icon-storage t)
